2323ed61a4f28e02f4d42e430e074eefe0a29b17
Florian Dormont <eiyeron@gmail.com>
2018-08-06 13:52:11 +0200

    Added code

--- README.md
+++ README.md
@@ -3,3 +3,30 @@ A Love2D library to make terminal-like stuff
 
 ## Note
 This is a WIP repo. I haven't studied or to make a proper library for Lua/Love2D.
+
+![Screenshot of the result of the following code](https://raw.githubusercontent.com/Eiyeron/LV-100/master/screenshot.png)
+
+```lua
+local font = love.graphics.newFont("x14y24pxHeadUpDaisy.ttf", 24) -- Thanks @hicchicc for the font
+local term = Terminal(14*80, (font:getHeight()-4)*25, font, nil, font:getHeight()-4)
+
+term:frame("line", 1,1,80,25)
+
+term:print(3,2,[[ ___      __   __         ____   _______  _______ ]])
+term:print(3,3,[[|   |    |  | |  |       |    | |  _    ||  _    |]])
+term:print(3,4,[[|   |    |  |_|  | ____   |   | | | |   || | |   |]])
+term:print(3,5,[[|   |    |       ||____|  |   | | | |   || | |   |]])
+term:print(3,6,[[|   |___ |       |        |   | | |_|   || |_|   |]])
+term:print(3,7,[[|       | |     |         |   | |       ||       |]])
+term:print(3,8,[[|_______|  |___|          |___| |_______||_______|]])
+
+term:print(55, 8, "By Eiyeron")
+
+term:print(4, 10, "-A terminal-ish library for Love2D--------------")
+
+term:print(4, 12, "Features")
+term:print(4, 14, "☛ Unicode support")
+term:print(4, 15, "☛ Slow terminal emulation")
+term:print(4, 16, "☛ Helpers")
+term:print(4, 16, "☛ Settings (speed, dimensions, font, ...)")
+```
---
+++ terminal.lua
@@ -0,0 +1,257 @@
+-- LV-100
+
+-- TODO : add licence
+
+local utf8 = require("utf8")
+
+-- TODO : properly quote the creator of this snippet
+local function utf8_sub(s,i,j)
+    i=utf8.offset(s,i)
+    j=utf8.offset(s,j+1)-1
+    return string.sub(s,i,j)
+end
+
+local function terminal_hide_cursor(terminal)
+    table.insert(terminal.stdin, {type="hide_cursor"})
+end
+
+local function terminal_show_cursor(terminal)
+    table.insert(terminal.stdin, {type="show_cursor"})
+end
+
+local function terminal_move_to(terminal, x, y)
+    table.insert(terminal.stdin, {type="move", x=math.floor(x), y=math.floor(y)})
+end
+
+local function terminal_clear(terminal, x, y, w, h)
+    x = x or 1
+    y = y or 1
+    w = w or terminal.width
+    h = h or terminal.height
+    table.insert(terminal.stdin, {type="clear", x=x, y=y, w=w, h=h})
+end
+
+local line_style = "┌┐└┘─│"
+local thick_style = "┏┓┗┛━┃"
+local block_style = "██████"
+local function terminal_frame(terminal, style, x, y, w, h)
+    table.insert(terminal.stdin, {type="frame", style=style, x=math.floor(x), y=math.floor(y), w=math.floor(w), h=math.floor(h)})
+end
+
+local function terminal_roll_up(terminal, how_many)
+    for times=1,how_many do
+        local first_row = terminal.buffer[1]
+        for y=2,terminal.height do
+            terminal.buffer[y-1] = terminal.buffer[y]
+        end
+        terminal.buffer[terminal.height] = first_row
+        for i=1,terminal.width do
+            first_row[i] = ' '
+        end
+    end
+end
+
+local function wrap_if_bottom(terminal)
+    if terminal.cursor_y > terminal.height then
+        terminal_roll_up(terminal, terminal.cursor_y - terminal.height)
+        terminal.cursor_y = terminal.height
+    end
+end
+
+local function terminal_update(terminal, dt)
+    if #terminal.stdin == 0 then return end
+    local frame_budget = terminal.speed * dt + terminal.accumulator
+    local stdin_index = 1
+    while frame_budget > terminal.char_cost do
+        local char_or_command = terminal.stdin[stdin_index]
+        if char_or_command == nil then break end
+        stdin_index = stdin_index + 1
+        frame_budget = frame_budget - terminal.char_cost
+
+        if type(char_or_command) == "string" then
+            if char_or_command == '\b' then
+                terminal.cursor_x = math.max(terminal.cursor_x - 1, 1)
+            elseif char_or_command == '\n' then
+                terminal.cursor_x = 1
+                terminal.cursor_y = terminal.cursor_y + 1
+                wrap_if_bottom(terminal)
+            else
+                terminal.buffer[terminal.cursor_y][terminal.cursor_x] = char_or_command
+                terminal.cursor_x = terminal.cursor_x + 1
+                if terminal.cursor_x > terminal.width then
+                    terminal.cursor_x = 1
+                    terminal.cursor_y = terminal.cursor_y + 1
+                    wrap_if_bottom(terminal)
+                end
+                terminal.dirty = true
+            end
+        elseif char_or_command.type == "clear" then
+            local x,y,w,h = char_or_command.x or 1, char_or_command.y or 1, char_or_command.w or terminal.width, char_or_command.h or terminal.h
+            for y=y,y+h-1 do
+                for x=x,x+w-1 do
+                    terminal.buffer[y][x] = " "
+                end
+            end
+            terminal.dirty = true
+        elseif char_or_command.type == "hide_cursor" then
+            terminal.show_cursor = false
+        elseif char_or_command.type == "show_cursor" then
+            terminal.show_cursor = true
+        elseif char_or_command.type == "move" then
+            terminal.cursor_x = char_or_command.x
+            terminal.cursor_y = char_or_command.y
+        elseif char_or_command.type == "save" then
+            terminal.saved_cursor_x = terminal.cursor_x
+            terminal.saved_cursor_y = terminal.cursor_y
+        elseif char_or_command.type == "load" then
+            terminal.cursor_x = terminal.saved_cursor_x
+            terminal.cursor_y = terminal.saved_cursor_y
+        elseif char_or_command.type == "frame" then
+            local style = char_or_command.style
+            if style == "line" then
+                style = line_style
+            elseif style == "thick" then
+                style = thick_style
+            elseif style == "block" then
+                style = block_style
+            else
+                assert(false, string.format("Unrecognized style %s", style))
+            end
+
+            local buffer = terminal.buffer
+            local x,y,width,height = char_or_command.x, char_or_command.y, char_or_command.w, char_or_command.h
+            buffer[y][x] = utf8_sub(style,1, 1)
+            buffer[y][x+width-1] = utf8_sub(style,2, 2)
+            buffer[y+height-1][x] = utf8_sub(style,3, 3)
+            buffer[y+height-1][x+width-1] = utf8_sub(style,4, 4)
+            for i=x+1, x+width-2 do
+                buffer[y][i] = utf8_sub(style,5, 5)
+                buffer[y+height-1][i] = utf8_sub(style,5, 5)
+            end
+            for i=y+1, y+height-2 do
+                buffer[i][x] = utf8_sub(style,6, 6)
+                buffer[i][x+width-1] = utf8_sub(style,6, 6)
+            end
+            terminal.dirty = true
+        else
+            assert(false, "Unrecognized command", char_or_command.type)
+        end
+    end
+    terminal.accumulator = frame_budget
+    local rest = {}
+    for i=stdin_index,#terminal.stdin do
+        table.insert(rest, terminal.stdin[i])
+    end
+    terminal.stdin = rest
+end
+
+local function terminal_draw(terminal)
+    local char_width, char_height = terminal.char_width, terminal.char_height
+    if terminal.dirty then
+        local previous_canvas = love.graphics.getCanvas()
+
+        love.graphics.push()
+        love.graphics.origin()
+
+        love.graphics.setCanvas(terminal.canvas)
+        love.graphics.clear(0,0,0)
+        love.graphics.setFont(terminal.font)
+        for y,row in ipairs(terminal.buffer) do
+            for x,char in ipairs(row) do
+                love.graphics.print(char, (x-1)*char_width, (y-1)*char_height)
+            end
+        end
+        terminal.dirty = false
+        love.graphics.pop()
+
+        love.graphics.setCanvas(previous_canvas)
+    end
+
+    love.graphics.draw(terminal.canvas)
+    if terminal.show_cursor then
+        love.graphics.setFont(terminal.font)
+            if love.timer.getTime()%1 > 0.5 then
+            love.graphics.print("_", (terminal.cursor_x-1) * char_width, (terminal.cursor_y -1) * char_height)
+        end
+    end
+end
+
+local function terminal_print(terminal, x, y, ...)
+    local res_string = nil
+
+    -- argument processing
+    -- shortcut : no coordinates => print at cursor position
+    if type(x) == "string" then
+        res_string = x
+    else
+        terminal:move_to(x, y)
+        res_string = string.format(...)
+    end
+
+    for i,p in utf8.codes(res_string) do
+        table.insert(terminal.stdin, utf8.char(p))
+    end
+end
+
+local function terminal_save_position(terminal)
+    table.insert(terminal.stdin, {type="save"})
+
+end
+
+local function terminal_load_position(terminal)
+    table.insert(terminal.stdin, {type="load"})
+end
+
+local function terminal (width, height, font, custom_char_width, custom_char_height)
+    local char_width = custom_char_width or font:getWidth('█')
+    local char_height = custom_char_height or font:getHeight()
+    local num_columns = math.floor(width/char_width)
+    local num_rows = math.floor(height/char_height)
+    local instance = {
+        width = math.floor(num_columns),
+        height = math.floor(num_rows),
+        font = font,
+
+        show_cursor = true,
+        cursor_x = 1,
+        cursor_y = 1,
+        saved_cursor_x = 1,
+        saved_cursor_y = 1,
+
+        dirty = false,
+        char_width = char_width,
+        char_height = char_height,
+
+        speed = 800,
+        char_cost = 1,
+        accumulator = 0,
+        stdin = {},
+
+        canvas = love.graphics.newCanvas(width, height),
+        buffer = {}
+    }
+
+    for i=1,num_rows do
+        local row = {}
+        for j=1,num_columns do
+            row[j] = ' '
+        end
+        instance.buffer[i] = row
+    end
+
+    instance.update = terminal_update
+    instance.draw = terminal_draw
+    instance.print = terminal_print
+    instance.clear = terminal_clear
+    instance.save_position = terminal_save_position
+    instance.load_position = terminal_load_position
+    instance.move_to = terminal_move_to
+    instance.hide_cursor = terminal_hide_cursor
+    instance.show_cursor = terminal_show_cursor
+
+    instance.frame = terminal_frame
+
+    return instance
+end
+
+return terminal
